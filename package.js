c03b4c510bd83e8c1636b1e76b15de70f3aab2cd
Orlando Martinez <orlanmarti@gmail.com>
2015-09-14 11:26:26 +0200

    fixed messages html class

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'garbolino:meteor-messages',
   summary: 'A pattern to display application messages to the user',
-  version: '1.0.0',
+  version: '1.0.2',
   author: "Orlando Martinez <orlando@rollingmute.com>",
   git: ' https://github.com/garbolino/meteor-messages.git'
 });
@@ -9,12 +9,12 @@ Package.describe({
 Package.on_use(function(api, where) {
   api.versionsFrom('METEOR@0.9.0');
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
-  api.add_files(['meteor-messages.js','messages.html', 'messages.js'], 'client');
+  api.add_files(['meteor-messages.js','messages.html', 'messages.js', 'messages.css'], 'client');
   if (api.export) api.export('Messages');
 });
 
 Package.onTest(function(api) {
-  api.use('garbolino:meteor-messages', 'client'); 
+  api.use('garbolino:meteor-messages', 'client');
   api.use(['tinytest', 'test-helpers'], 'client');
   api.add_files('meteor-messages-tests.js', 'client');
 });
